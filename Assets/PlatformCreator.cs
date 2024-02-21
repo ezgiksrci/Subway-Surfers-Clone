84dc540d5b1999c92459cb44ff21a11df1991987
Ezgi Keserci <keserci.ezgi@gmail.com>
2024-02-21 18:20:44 +0300

    Add Random Environment

---
+++ Assets/PlatformCreator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformCreator : MonoBehaviour
+{
+    [SerializeField] List<GameObject> allPlatforms;
+    [SerializeField] List<GameObject> movingPlatforms;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            AddPlatformToMovingList(SelectRandomPlatform());
+            RemovePlatformFromMovingList(other.gameObject);
+        }
+    }
+
+    private void AddPlatformToMovingList(GameObject newPlatform)
+    {
+        movingPlatforms.Add(newPlatform);
+        newPlatform.transform.position = new Vector3(0, 0, 207);
+        newPlatform.SetActive(true);
+    }
+    private void RemovePlatformFromMovingList(GameObject oldPlatform)
+    {
+        oldPlatform.SetActive(false);
+        movingPlatforms.Remove(oldPlatform);
+    }
+
+    private GameObject SelectRandomPlatform()
+    {
+        GameObject newPlatform;
+
+        do
+        {
+            newPlatform = allPlatforms[UnityEngine.Random.Range(0, allPlatforms.Count)];
+        } while (movingPlatforms.Contains(newPlatform));
+
+        return newPlatform;
+    }
+}
