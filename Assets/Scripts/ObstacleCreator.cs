790156c77e55760e67ac364382ea855167d5356c
Ezgi Keserci <keserci.ezgi@gmail.com>
2024-02-25 17:42:05 +0300

    Refactor Procedural Generation

---
+++ Assets/Scripts/ObstacleCreator.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ObstacleType
+{
+    Surmountable,
+    NonSurmountable
+}
+
+public class ObstacleCreator : MonoBehaviour
+{
+    [SerializeField] private ObstacleType _obstacleType;
+    [SerializeField] private List<Transform> _obstaclePool;
+
+    private void OnEnable()
+    {
+        DisableAllObstacles();
+        EnableRandomObstacle();
+    }
+
+    private void DisableAllObstacles()
+    {
+        foreach (var obstacle in _obstaclePool)
+        {
+            if (obstacle.gameObject.activeSelf)
+            {
+                obstacle.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void EnableRandomObstacle()
+    {
+        Transform randomObstacle = _obstaclePool[UnityEngine.Random.Range(0, _obstaclePool.Count)];
+        randomObstacle.gameObject.SetActive(true);
+    }
+}
